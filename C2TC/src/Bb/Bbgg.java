1c3db65a98e2835c0f2e8f49f4815365a423502e
HP <HP@LAPTOP-4AVDLV80>
2023-07-30 11:31:07 +0530

    day1

--- C2TC/src/Bb/Bbgg.java
+++
@@ -1,5 +0,0 @@
-package Bb;
-
-public class Bbgg {
-
-}
---
+++ C2TC/src/day01/type.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class type {
+
+	public static void main(String[] args) {
+		int a=50;
+		float b=a;
+		System.out.println(b);
+
+	}
+
+}
